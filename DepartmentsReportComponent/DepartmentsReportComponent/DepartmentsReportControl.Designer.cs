d4fd24b925456ef1d692fca36d63a42eccd4ab65
Ivan Gutorov <gutorov.ia1@gmail.com>
2025-10-16 06:20:35 +0400

    fix some dirty shit bruh

--- DepartmentsReportComponent/DepartmentsReportComponent/DepartmentsReportControl.Designer.cs
+++ DepartmentsReportComponent/DepartmentsReportComponent/DepartmentsReportControl.Designer.cs
@@ -31,7 +31,10 @@
             departmentsReportDataGridView = new DataGridView();
             specializationComboBox = new ComboBox();
             applyButton = new Button();
+            departmentsReportGroupBox = new GroupBox();
+            specializationLabel = new Label();
             ((System.ComponentModel.ISupportInitialize)departmentsReportDataGridView).BeginInit();
+            departmentsReportGroupBox.SuspendLayout();
             SuspendLayout();
             // 
             // departmentsReportDataGridView
@@ -40,43 +43,66 @@
             departmentsReportDataGridView.AllowUserToDeleteRows = false;
             departmentsReportDataGridView.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
             departmentsReportDataGridView.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            departmentsReportDataGridView.Dock = DockStyle.Bottom;
-            departmentsReportDataGridView.Location = new Point(0, 32);
+            departmentsReportDataGridView.Dock = DockStyle.Fill;
+            departmentsReportDataGridView.Location = new Point(3, 19);
             departmentsReportDataGridView.Name = "departmentsReportDataGridView";
             departmentsReportDataGridView.ReadOnly = true;
             departmentsReportDataGridView.RowHeadersVisible = false;
-            departmentsReportDataGridView.Size = new Size(240, 170);
+            departmentsReportDataGridView.Size = new Size(585, 272);
             departmentsReportDataGridView.TabIndex = 0;
             // 
             // specializationComboBox
             // 
             specializationComboBox.FormattingEnabled = true;
-            specializationComboBox.Location = new Point(3, 3);
+            specializationComboBox.Location = new Point(3, 18);
             specializationComboBox.Name = "specializationComboBox";
-            specializationComboBox.Size = new Size(121, 23);
+            specializationComboBox.Size = new Size(150, 23);
             specializationComboBox.TabIndex = 1;
             // 
             // applyButton
             // 
-            applyButton.Location = new Point(130, 3);
+            applyButton.Location = new Point(3, 47);
             applyButton.Name = "applyButton";
-            applyButton.Size = new Size(107, 23);
+            applyButton.Size = new Size(150, 23);
             applyButton.TabIndex = 2;
             applyButton.Text = "Сформировать";
             applyButton.UseVisualStyleBackColor = true;
             applyButton.Click += ApplyButton_Click;
             // 
+            // departmentsReportGroupBox
+            // 
+            departmentsReportGroupBox.Controls.Add(departmentsReportDataGridView);
+            departmentsReportGroupBox.Dock = DockStyle.Right;
+            departmentsReportGroupBox.Location = new Point(159, 0);
+            departmentsReportGroupBox.Name = "departmentsReportGroupBox";
+            departmentsReportGroupBox.Size = new Size(591, 294);
+            departmentsReportGroupBox.TabIndex = 3;
+            departmentsReportGroupBox.TabStop = false;
+            departmentsReportGroupBox.Text = "Отчёт по подразделениям";
+            // 
+            // specializationLabel
+            // 
+            specializationLabel.AutoSize = true;
+            specializationLabel.Location = new Point(3, 0);
+            specializationLabel.Name = "specializationLabel";
+            specializationLabel.Size = new Size(93, 15);
+            specializationLabel.TabIndex = 4;
+            specializationLabel.Text = "Специализация";
+            // 
             // DepartmentsReportControl
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
+            Controls.Add(specializationLabel);
+            Controls.Add(departmentsReportGroupBox);
             Controls.Add(applyButton);
             Controls.Add(specializationComboBox);
-            Controls.Add(departmentsReportDataGridView);
             Name = "DepartmentsReportControl";
-            Size = new Size(240, 202);
+            Size = new Size(750, 294);
             ((System.ComponentModel.ISupportInitialize)departmentsReportDataGridView).EndInit();
+            departmentsReportGroupBox.ResumeLayout(false);
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
@@ -84,5 +110,7 @@
         private DataGridView departmentsReportDataGridView;
         private ComboBox specializationComboBox;
         private Button applyButton;
+        private GroupBox departmentsReportGroupBox;
+        private Label specializationLabel;
     }
 }
